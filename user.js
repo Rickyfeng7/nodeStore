be45ec69bc2e481b6b3c79a9b3c9afeaa7e2b86f
Ricky Feng <34487908+Rickyfeng7@users.noreply.github.com>
2018-02-26 08:19:38 -0800

    fixing indent

--- user.js
+++ user.js
@@ -3,18 +3,18 @@ var mysql = require("mysql");
 var Table = require("cli-table");
 var password = require ("./keys.js")
 var connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
+host: "localhost",
+port: 3306,
 
-	user: "root",
+user: "root",
 
-	password: password,
-	database: "salesdb"
+password: password,
+database: "salesdb"
 })
 
 connection.connect(function(err) {
-	if (err) throw err;
-	 // console.log("Connected as id: " + connection.threadId + "\n");
+  if (err) throw err;
+// console.log("Connected as id: " + connection.threadId + "\n");
 });
 
 // function start(){
@@ -50,74 +50,71 @@ connection.connect(function(err) {
 
 // start();
 customerScreen();
-function customerScreen(){
-	connection.query("SELECT * FROM products", function(err, res){
-	if (err) console.log(err)
-	var table = new Table({
-	head:["id", "player", "sport", "department", "price", "stock"]
-	})
-	for(var i = 0; i < res.length; i++){
-		table.push([res[i].id, res[i].player, res[i].sport, res[i].department, res[i].price, res[i].stock]
-		)
-	}
-	console.log("\n" + table.toString());
-	buyer();
-	})
+  function customerScreen(){
+    connection.query("SELECT * FROM products", function(err, res){
+      if (err) console.log(err)
+      var table = new Table({
+        head:["id", "player", "sport", "department", "price", "stock"]
+      })
+      for(var i = 0; i < res.length; i++){
+        table.push([res[i].id, res[i].player, res[i].sport, res[i].department, res[i].price, res[i].stock]
+        )
+      }
+    console.log("\n" + table.toString());
+    buyer();
+  })
 };
 
 function buyer(){
-connection.query("SELECT * FROM products", function(err, results) {
+  connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
     inquirer
-      .prompt([
+    .prompt([
+      {
+        name: "choice",
+        type: "rawlist",
+        choices: function() {
+        var choiceArray = [];
+        for (var i = 0; i < results.length; i++) {
+        choiceArray.push(results[i].player);
+      }
+        return choiceArray;
+      },
+        message: "what do you wanna buy ?"
+      },
+      {
+        name: "userQuantity",
+        type: "input",
+        message: "How much would you want to buy?"
+      }
+    ])
+    .then(function(answer) {
+    var chosenItem;
+    for (var i = 0; i < results.length; i++) {
+      if (results[i].player === answer.choice) {
+        chosenItem = results[i];
+      }
+      }
+      if (chosenItem.stock >= parseInt(answer.userQuantity)) { var itemsStock = chosenItem.stock - parseInt(answer.userQuantity)
+      connection.query("UPDATE products SET ? WHERE ?",
+      [
         {
-			name: "choice",
-			type: "rawlist",
-			choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].player);
-            }
-            return choiceArray;
-          },
-          message: "what do you wanna buy ?"
+          stock: itemsStock
         },
         {
-          name: "userQuantity",
-          type: "input",
-          message: "How much would you want to buy?"
+          id: chosenItem.id
         }
-      ])
-      .then(function(answer) {
-        var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].player === answer.choice) {
-            chosenItem = results[i];
-          }
+      ],
+        function(error) {
+          if (error) throw err;
+          console.log("you have successfully!" + chosenItem.player + " jersey" );
+          customerScreen();
         }
-
-        if (chosenItem.stock >= parseInt(answer.userQuantity)) { var itemsStock = chosenItem.stock - parseInt(answer.userQuantity)
-          
-          connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-              {
-                stock: itemsStock
-              },
-              {
-                id: chosenItem.id
-              }
-            ],
-            function(error) {
-              if (error) throw err;
-              console.log("you have successfully!" + chosenItem.player + " jersey" );
-              customerScreen();
-            }
-          );
-        }
+      );
+      }
         else {
           console.log("not enough in stock");
         }
-      });
+    });
   });
 }
